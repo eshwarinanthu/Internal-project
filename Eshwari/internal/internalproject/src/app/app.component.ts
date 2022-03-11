42a18d99d1cf3f08b08d4a1f139cf4c893cc990d
eshwarinanthu <eshwarinanthu@gmail.com>
2022-03-11 17:17:59 +0530

    internal-project-commit

---
+++ Eshwari/internal/internalproject/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'internalproject';
+  logo = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fseeklogo.com%2Fimages%2FT%2FTVM_verzekeringen-logo-9525602A3E-seeklogo.com.gif&imgrefurl=https%3A%2F%2Fseeklogo.com%2Ffree-vector-logos%2Ftvm&tbnid=wCfCRaW2CPmuLM&vet=12ahUKEwiP1MOh0b32AhUlRmwGHbX1DC4QMygFegUIARDEAQ..i&docid=JUdRpm2ySKlQ-M&w=200&h=200&q=tvm%20logo%20png&ved=2ahUKEwiP1MOh0b32AhUlRmwGHbX1DC4QMygFegUIARDEAQ"
+}
